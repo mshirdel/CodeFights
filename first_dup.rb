60c2fc6d5e41379a0dcc2724494c78212ad92f50
Shirdel <mshirdel@gmail.com>
2017-09-02 17:08:40 +0430

    find best solution for time limit but test not passed by codefights

--- first_dup.rb
+++ first_dup.rb
@@ -2,13 +2,13 @@ def firstDuplicate(a)
     temp = []
     duplicates = []
     a.each do |n|
-       duplicates << n  if temp.include?(n) 
-       temp << n unless temp.include?(n)
+       duplicates << n  if temp.bsearch {|x| x == n} 
+       temp << n unless temp.bsearch {|x| x == n} 
     end
     return -1 if duplicates.count == 0
     return duplicates.first unless duplicates.count == 0
- end
+end
 a=[]
-(10**5).times { |n| a << (rand*10).to_i + 1 }
-puts a.count
-# puts firstDuplicate a
+limit = 100_000
+limit.times { |n| a << rand(1..limit) }
+puts firstDuplicate a
--- first_dup_set.rb
+++ first_dup_set.rb
@@ -1,33 +1,37 @@
 require 'benchmark'
 
-def firstDuplicateHash(a)
-    temp = {}
+def firstDuplicate(a)
+    temp = []
     duplicates = []
-    a.each_with_index do |n, i|
-        duplicates << n  if temp.keys.include?(n) 
-        temp[n] = i unless temp.keys.include?(n)
+    a.each do |n|
+       duplicates << n  if temp.bsearch {|x| x == n} 
+       temp << n unless temp.bsearch {|x| x == n} 
     end
     return -1 if duplicates.count == 0
     return duplicates.first unless duplicates.count == 0
 end
 
-def firstDuplicate(a)
+def firstDuplicateBSearch(a)
     temp = []
     duplicates = []
     a.each do |n|
-       duplicates << n  if temp.include?(n) 
-       temp << n unless temp.include?(n)
+        duplicates << n  if temp.include?(n) 
+        temp << n unless temp.include?(n)
     end
     return -1 if duplicates.count == 0
     return duplicates.first unless duplicates.count == 0
- end
+end
 
 a=[]
-limit = 20000
+limit = 100_000
 limit.times { |n| a << rand(1..limit) }
 
-Benchmark.bm(7) do |x|
-    x.report("array: ") do
+Benchmark.bm(8) do |x|
+    x.report("bsearch : ") do
         results = firstDuplicate a
+    end
+
+    x.report("include? : ") do
+        results = firstDuplicateBSearch a
     end
 end
